a64da961ce95fe4b0fb101b68eaba125e9010f42
SophieBauernfeind <S1810458002@students.fh-hangenberg.at>
2021-03-31 09:23:59 +0200

    #3 removed unused modules

--- external/rest/src/main/java/science/aist/sushiya/external/rest/RestController.java
+++
@@ -1,9 +0,0 @@
-package science.aist.sushiya.external.rest;
-
-/**
- * Combined interface for CRUD and Query Methods
- *
- * @author Oliver Krauss on 17.03.2015.
- */
-public interface RestController<ID, DTO> extends RestCRUD<ID, DTO>, RestQueries<ID, DTO> {
-}
